c2dc3030e34a396e77cfc485cbb59657221109b0
Holden Folk <holden@kaidu.ai>
2024-07-23 15:07:56 -0400

    Change geolocation config to be able to select exact locations

--- src/components/molecule/GeolocationDisplay.tsx
+++ src/components/molecule/GeolocationDisplay.tsx
@@ -9,26 +9,6 @@ import { View } from '../atomic/View';
 import { Geolocation } from '@/types/api';
 import * as Progress from 'react-native-progress';
 
-const Container = styled(View)`
-	flex: 1;
-	padding: 20px;
-	justify-content: center;
-	background-color: ${({ theme }) => theme.colors.primary};
-`;
-
-const Map = styled(MapView)`
-	flex: 1;
-	margin-top: 20px;
-	border-radius: 20px;
-	overflow: hidden;
-	margin-bottom: 20px;
-`;
-
-const CenteredContainer = styled(View)`
-	flex-direction: row;
-	justify-content: center;
-`;
-
 interface Region {
 	latitude: number;
 	longitude: number;
@@ -48,6 +28,20 @@ const SetLocationScreen: React.FC<SetLocationScreenProps> = ({
 }) => {
 	const [region, setRegion] = useState<Region>();
 
+	// Function to check if two regions are approximately equal
+	const regionsAreEqual = (
+		region1: Region,
+		region2: Region,
+		tolerance = 0.0001,
+	) => {
+		return (
+			Math.abs(region1.latitude - region2.latitude) < tolerance &&
+			Math.abs(region1.longitude - region2.longitude) < tolerance &&
+			Math.abs(region1.latitudeDelta - region2.latitudeDelta) < tolerance &&
+			Math.abs(region1.longitudeDelta - region2.longitudeDelta) < tolerance
+		);
+	};
+
 	const handleGetCurrentLocation = async () => {
 		const result = await requestPreciseLocationPermission();
 		const enabled = result === 'granted';
@@ -60,12 +54,13 @@ const SetLocationScreen: React.FC<SetLocationScreenProps> = ({
 		}
 		Geo.getCurrentPosition(
 			position => {
-				setRegion({
+				const newRegion = {
 					latitude: position.coords.latitude,
 					longitude: position.coords.longitude,
 					latitudeDelta: 0.01,
 					longitudeDelta: 0.01,
-				});
+				};
+				setRegion(newRegion);
 				updateGlobalGeolocation({
 					latitude: position.coords.latitude,
 					longitude: position.coords.longitude,
@@ -95,6 +90,16 @@ const SetLocationScreen: React.FC<SetLocationScreenProps> = ({
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
+	const handleRegionChangeComplete = (newRegion: Region) => {
+		if (region && !regionsAreEqual(newRegion, region, 0.0001)) {
+			setRegion(newRegion);
+			updateGlobalGeolocation({
+				latitude: newRegion.latitude,
+				longitude: newRegion.longitude,
+			});
+		}
+	};
+
 	return (
 		<Container>
 			{region && (
@@ -106,7 +111,12 @@ const SetLocationScreen: React.FC<SetLocationScreenProps> = ({
 							type="outline"
 						/>
 					</CenteredContainer>
-					<Map provider={PROVIDER_GOOGLE} region={region} mapType="standard">
+					<Map
+						provider={PROVIDER_GOOGLE}
+						region={region}
+						onRegionChangeComplete={handleRegionChangeComplete}
+						mapType="standard"
+					>
 						<Marker
 							coordinate={{
 								latitude: region.latitude,
@@ -129,3 +139,23 @@ const SetLocationScreen: React.FC<SetLocationScreenProps> = ({
 };
 
 export default SetLocationScreen;
+
+const Container = styled(View)`
+	flex: 1;
+	padding: 20px;
+	justify-content: center;
+	background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Map = styled(MapView)`
+	flex: 1;
+	margin-top: 20px;
+	border-radius: 20px;
+	overflow: hidden;
+	margin-bottom: 20px;
+`;
+
+const CenteredContainer = styled(View)`
+	flex-direction: row;
+	justify-content: center;
+`;
